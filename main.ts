7638bd2dd670024231f60201c473f46b310025b6
svi-berlin <svi-berlin@users.noreply.github.com>
2025-07-12 16:09:08 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,32 @@
 input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
-    serialmp3.playMp3Track(3, 1)
+    serialmp3.playMp3Track(1, 6)
     basic.setLedColor(0x00ff00)
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    serialmp3.playMp3Track(1, 1)
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showIcon(IconNames.Happy)
 })
 input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
-    serialmp3.playMp3Track(2, 1)
+    serialmp3.playMp3Track(4, 6)
     basic.setLedColor(0x00ff00)
 })
 input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
-    serialmp3.playMp3Track(4, 1)
+    serialmp3.playMp3Track(2, 6)
+    basic.setLedColor(0x00ff00)
+})
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+    basic.setLedColor(0xff0000)
+    serialmp3.playMp3Track(3, 6)
     basic.setLedColor(0x00ff00)
 })
 basic.setLedColor(0xff0000)
 serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
 serialmp3.setMp3Volume(30)
-serialmp3.playMp3Track(1, 1)
 basic.setLedColor(0x00ff00)
 basic.forever(function () {
 	
