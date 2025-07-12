1bb097c5ed99724efd424bfdb9d2e8901564fd48
svi-berlin <svi-berlin@users.noreply.github.com>
2025-07-12 16:33:19 +0200

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
-    serialmp3.playMp3Track(1, 6)
+    serialmp3.playMp3Track(5, 6)
     basic.setLedColor(0x00ff00)
 })
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showIcon(IconNames.Heart)
+    serialmp3.playMp3Track(1, 6)
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     basic.showIcon(IconNames.Happy)
+    serialmp3.playMp3Track(1, 6)
 })
 input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
@@ -16,12 +18,12 @@ input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
 })
 input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
-    serialmp3.playMp3Track(2, 6)
+    serialmp3.playMp3Track(6, 6)
     basic.setLedColor(0x00ff00)
 })
 input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
     basic.setLedColor(0xff0000)
-    serialmp3.playMp3Track(3, 6)
+    serialmp3.playMp3Track(7, 6)
     basic.setLedColor(0x00ff00)
 })
 basic.setLedColor(0xff0000)
